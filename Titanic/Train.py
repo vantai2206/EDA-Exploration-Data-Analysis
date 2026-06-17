aa46194618203699b70a16a17808d3ef4685d055
vantai2206 <tranvantai.qbh1@gmail.com>
2026-06-17 11:36:31 +0700

    Analysis between Survived and all columns Categorical

--- Titanic/Train.py
+++ Titanic/Train.py
@@ -11,19 +11,27 @@ from pandas.core.reshape import tile
     # Mix Type of data: Age, Ticket , Cabin
     # Contain Errors    : Name
     # Blank or Null : Cabin > Ticket > Age > Embarked
+
 # lay du liệu train
 train = pd.read_csv("train.csv")
+
 # biet ten tung columns
 print(train.columns)
+
 # hien thi nhung the firts row
 print(train.head())
+
 # Gop PassengerID thanh index
+
 train.set_index(train.PassengerId,inplace=True)
 train.drop("PassengerId",axis=1,inplace=True)
+
 #kieu du lieu trong table
 print(train.info())
+
 #thay doi kieu dư lieu
 train['Survived'] = train['Survived'].astype('category')
+
 #Thay doi kieu du lieu cung luc
 convert = ['Sex','Pclass','SibSp','Parch']
 def con_vert(df,convert):
@@ -31,19 +39,26 @@ def con_vert(df,convert):
         df[col] = df[col].astype('category')
 con_vert(train,convert)
 print(train.info())
+
 #mo ta cai numerical features value
 print(train.describe())
+
 #mo ta cai categorical feature value
 print(train.describe(include=['category']))
+
 # Kiem tra gia tri null in the table
 print(train.isnull().sum())
+
 #Xem có bao nhiêu người sống sót trên tàu
 print(train['Survived'].value_counts().to_frame())
+
 #Bao nhiêu phần trăm số người sống sót
 print(train['Survived'].value_counts(normalize=True).to_frame())
+
 #Seaborn
 #sns.countplot(data=train,x="Survived",hue='Sex')
 #plt.show()
+
 #Xem tất cả các cột categorical sống sót như nào>
 col = ['Pclass','SibSp','Sex','Embarked','Parch']
 n_row = 2
