fac0c1e85fdf55034926bf48cfdcaf4381ee7c4d
vantai2206 <tranvantai.qbh1@gmail.com>
2026-06-17 11:30:25 +0700

    Analysis all columns for Surived

--- Titanic/Train.py
+++ Titanic/Train.py
@@ -2,8 +2,11 @@
 import pandas as pd
 import numpy as np
 from pandas.core.groupby import categorical
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas.core.reshape import tile
 
-    # Categorical : Survived,Sex,Embarked,Pclass,SibSp,Parch
+# Categorical : Survived,Sex,Embarked,Pclass,SibSp,Parch
     # Numerical : (continuous)Age , Fare ( discrete)
     # Mix Type of data: Age, Ticket , Cabin
     # Contain Errors    : Name
@@ -34,3 +37,26 @@ print(train.describe())
 print(train.describe(include=['category']))
 # Kiem tra gia tri null in the table
 print(train.isnull().sum())
+#Xem có bao nhiêu người sống sót trên tàu
+print(train['Survived'].value_counts().to_frame())
+#Bao nhiêu phần trăm số người sống sót
+print(train['Survived'].value_counts(normalize=True).to_frame())
+#Seaborn
+#sns.countplot(data=train,x="Survived",hue='Sex')
+#plt.show()
+#Xem tất cả các cột categorical sống sót như nào>
+col = ['Pclass','SibSp','Sex','Embarked','Parch']
+n_row = 2
+n_col = 3
+fig , ax = plt.subplots(n_row,n_col,figsize=(n_col*3.5,n_row*3.5))
+for r in range(0,n_row):
+    for c in range(0,n_col):
+        i = r*n_col+c
+        if i < len(col):
+            ax_i = ax[r,c]
+            sns.countplot(data= train , x =col[i],hue = 'Survived',ax = ax_i)
+            ax_i.set_title(f"Figure {i+1}: Survived {col[i]}")
+            ax_i.legend(title='',loc='upper right',labels=['Not Survived','Survived'])
+ax.flat[-1].set_visible(False)
+plt.tight_layout()
+plt.show()
